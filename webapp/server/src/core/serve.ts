44b9f2078320d447ad2528e6fd1ec7822aa308ac
DenizUgur <deniz343@gmail.com>
2020-12-20 00:37:44 +0300

    midterm password generation & refactor

--- webapp/server/src/core/serve.ts
+++ webapp/server/src/core/serve.ts
@@ -7,114 +7,132 @@ import { FileAccess, Stats, User, UserAccess } from "../db";
 const router = Router();
 const exec_prom = util.promisify(exec);
 
-const isAvailable = async (type: string, level: number, object: boolean) => {
+const isAvailable = async (type: string, level: number) => {
 	try {
 		const file: any = await FileAccess.findByPk(type);
 		if (!file) return false;
 
 		if (file.level > level) return false;
 
-		if (object) return file;
-		return file.dataValues.enabled;
+		return file;
 	} catch (error) {
 		console.error(error);
 		return false;
 	}
 };
 
-router.get("/", (req, res) => {
-	res.render("index", {
+router.get("/", async (req, res) => {
+	const type = req.session.type || process.env.FALLBACK_TYPE;
+	let file: any = await isAvailable(type, req.user.level);
+
+	if (!file.enabled)
+		return res.render("index", {
+			message: "This homework is not available yet.",
+			serve: false,
+		});
+
+	if (type == "mt2") {
+		let password: any =
+			(parseInt(req.user.sid.split("S")[1]) * 48271) %
+			(Math.pow(2, 31) - 1);
+		password = password.toString();
+		password = parseInt(password.substr(password.length - 5));
+
+		return res.render("index", {
+			message: `Hi ${req.user.displayName}, your file is currently being prepared. Please wait... Password: ${password}`,
+			serve: true,
+		});
+	}
+	return res.render("index", {
 		message: `Hi ${req.user.displayName}, your file is currently being prepared. Please wait...`,
 		serve: true,
 	});
 });
 
 router.get("/download/:type", async (req, res) => {
-	//TODO: Change fallback behavior
-	const type = req.session.type || "hw5";
+	const type = req.session.type || process.env.FALLBACK_TYPE;
 	await Stats.create({
 		ip: req.ip,
 		userAgent: req.headers["user-agent"],
 		type: req.session.type || "N/A",
 	});
 
-	let file: any = await isAvailable(type, req.user.level, true);
+	let file: any = await isAvailable(type, req.user.level);
 
-	if (file.enabled) {
-		//* Check user access
-		const user: any = await User.findOne({
-			where: {
-				email: req.user.email,
-			},
-			include: [UserAccess],
-		});
-
-		if (!user) {
-			throw new Error("Sanity Check: User not found!");
-		}
-
-		let access = user.user_accesses?.find(
-			(el: any) => el.dataValues.type === type
-		);
-
-		if (access?.accessed && file.onetime) {
-			return res.render("index", {
-				message: "You are not allowed to download more than once.",
-			});
-		}
-
-		if (access?.macrofree) {
-			exec_prom(`python3 ../worker/app.py ${type} 1 ${req.user.sid}`)
-				.then(() => {
-					res.download(
-						`../data/out/${req.user.sid}_${type}.xlsx`,
-						`${req.user.sid}_${type}.xlsx`,
-						async (error) => {
-							if (error) throw error;
-
-							await UserAccess.upsert({
-								accessed: true,
-								type: type,
-								userOid: user.dataValues.oid,
-							});
-							fs.unlinkSync(
-								`../data/out/${req.user.sid}_${type}.xlsx`
-							);
-						}
-					);
-				})
-				.catch((error) => {
-					throw error;
-				});
-		} else {
-			exec_prom(`python3 ../worker/app.py ${type} 0 ${req.user.sid}`)
-				.then(() => {
-					res.download(
-						`../data/out/${req.user.sid}_${type}.xlsm`,
-						`${req.user.sid}_${type}.xlsm`,
-						async (error) => {
-							if (error) throw error;
-
-							await UserAccess.upsert({
-								accessed: true,
-								type: type,
-								userOid: user.dataValues.oid,
-							});
-							fs.unlinkSync(
-								`../data/out/${req.user.sid}_${type}.xlsm`
-							);
-						}
-					);
-				})
-				.catch((error) => {
-					throw error;
-				});
-		}
-	} else {
+	if (!file.enabled)
 		return res.render("index", {
 			message: "This homework is not available yet.",
 			serve: false,
 		});
+
+	//* Check user access
+	const user: any = await User.findOne({
+		where: {
+			email: req.user.email,
+		},
+		include: [UserAccess],
+	});
+
+	if (!user) {
+		throw new Error("Sanity Check: User not found!");
+	}
+
+	let access = user.user_accesses?.find(
+		(el: any) => el.dataValues.type === type
+	);
+
+	if (access?.accessed && file.onetime) {
+		return res.render("index", {
+			message: "You are not allowed to download more than once.",
+		});
+	}
+
+	if (access?.macrofree) {
+		exec_prom(`python3 ../worker/app.py ${type} 1 ${req.user.sid}`)
+			.then(() => {
+				res.download(
+					`../data/out/${req.user.sid}_${type}.xlsx`,
+					`${req.user.sid}_${type}.xlsx`,
+					async (error) => {
+						if (error) throw error;
+
+						await UserAccess.upsert({
+							accessed: true,
+							type: type,
+							userOid: user.dataValues.oid,
+						});
+						fs.unlinkSync(
+							`../data/out/${req.user.sid}_${type}.xlsx`
+						);
+					}
+				);
+			})
+			.catch((error) => {
+				throw error;
+			});
+	} else {
+		exec_prom(`python3 ../worker/app.py ${type} 0 ${req.user.sid}`)
+			.then(() => {
+				res.download(
+					`../data/out/${req.user.sid}_${type}.xlsm`,
+					`${req.user.sid}_${type}.xlsm`,
+					async (error) => {
+						if (error) throw error;
+
+						await UserAccess.upsert({
+							accessed: true,
+							type: type,
+							userOid: user.dataValues.oid,
+						});
+						fs.unlinkSync(
+							`../data/out/${req.user.sid}_${type}.xlsm`
+						);
+					}
+				);
+			})
+			.catch((error) => {
+				throw error;
+			});
 	}
 });
 
